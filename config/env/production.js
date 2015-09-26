89694a835ba70af9aefa422ad9203d55b534a8f2
Sébastien Combéfis <seb478@gmail.com>
2015-09-26 22:58:36 +0200

    Update node libraries for production environment

--- config/env/production.js
+++ config/env/production.js
@@ -5,19 +5,36 @@ module.exports = {
 	assets: {
 		lib: {
 			css: [
-				'public/lib/bootstrap/dist/css/bootstrap.min.css',
-				'public/lib/bootstrap/dist/css/bootstrap-theme.min.css',
+				'public/lib/bootstrap/dist/css/bootstrap.css',
+				'public/lib/bootstrap/dist/css/bootstrap-theme.css',
+				'public/lib/ng-tags-input/ng-tags-input.css',
+				'public/lib/ng-tags-input/ng-tags-input.bootstrap.css',
+				'public/lib/angular-bootstrap-datetimepicker/src/css/datetimepicker.css'
 			],
 			js: [
-				'public/lib/angular/angular.min.js',
-				'public/lib/angular-resource/angular-resource.js', 
-				'public/lib/angular-cookies/angular-cookies.js', 
-				'public/lib/angular-animate/angular-animate.js', 
-				'public/lib/angular-touch/angular-touch.js', 
-				'public/lib/angular-sanitize/angular-sanitize.js', 
-				'public/lib/angular-ui-router/release/angular-ui-router.min.js',
+				'public/lib/angular/angular.js',
+				'public/lib/angular-animate/angular-animate.js',
+				'public/lib/angular-bootstrap/ui-bootstrap-tpls.js',
+				'public/lib/angular-cookies/angular-cookies.js',
+				'public/lib/angular-mocks/angular-mocks.js',
+				'public/lib/moment/moment.js',
+				'public/lib/angular-moment/angular-moment.js',
+				'public/lib/angular-resource/angular-resource.js',
+				'public/lib/angular-sanitize/angular-sanitize.js',
+				'public/lib/angular-touch/angular-touch.js',
+				'public/lib/angular-ui-event/dist/event.js',
+				'public/lib/angular-ui-indeterminate/dist/indeterminate.js',
+				'public/lib/angular-ui-mask/dist/mask.js',
+				'public/lib/angular-ui-router/release/angular-ui-router.js',
+				'public/lib/angular-ui-scroll/dist/ui-scroll.js',
+				'public/lib/angular-ui-scrollpoint/dist/scrollpoint.js',
+				'public/lib/angular-ui-uploader/dist/uploader.js',
+				'public/lib/angular-ui-validate/dist/validate.js',
 				'public/lib/angular-ui-utils/index.js',
-				'public/lib/angular-bootstrap/ui-bootstrap-tpls.min.js'
+				'public/lib/jquery/dist/jquery.js',
+				'public/lib/ng-tags-input/ng-tags-input.js',
+				'public/lib/bootstrap/dist/js/bootstrap.js',
+				'public/lib/angular-bootstrap-datetimepicker/src/js/datetimepicker.js'
 			]
 		},
 		css: 'public/dist/application.min.css',
