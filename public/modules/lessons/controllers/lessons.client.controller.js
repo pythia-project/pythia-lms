2a669310a6f5b917180ceae0bdd421be32dcdd03
Sébastien Combéfis <seb478@gmail.com>
2015-10-12 18:59:22 +0200

    Add statistics button to lesson page

--- public/modules/lessons/controllers/lessons.client.controller.js
+++ public/modules/lessons/controllers/lessons.client.controller.js
@@ -265,4 +265,18 @@ angular.module('lessons').controller('LessonsController', ['$scope', '$statePara
 		}
 		return tab;
 	};
+
+	// Test whether the current user has one of the specified roles
+	$scope.hasRole = function(roles) {
+		return $scope.authentication.user && roles.some(function(element, index, array) {
+			return $scope.authentication.user.roles.indexOf(element) !== -1;
+		});
+	};
+
+	// Test whether the current user is a coordinator of this course
+	$scope.isCoordinator = function() {
+		return $scope.authentication.user && $scope.course.coordinators.some(function(element, index, array) {
+			return $scope.authentication.user._id === element._id;
+		});
+	};
 }]);
