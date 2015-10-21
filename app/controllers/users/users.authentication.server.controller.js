4affe06ff7526acbe63258e817c79138130af2f7
Sébastien Combéfis <seb478@gmail.com>
2015-10-21 20:07:52 +0200

    Change signup procedure
    - User not logged in anymore after successful account creation
    - A confirmation message of successful account creation is shown

--- app/controllers/users/users.authentication.server.controller.js
+++ app/controllers/users/users.authentication.server.controller.js
@@ -34,13 +34,7 @@ exports.signup = function(req, res) {
 		// Remove sensitive data before login
 		user.password = undefined;
 		user.salt = undefined;
-		req.login(user, function(err) {
-			if (err) {
-				res.status(400).send(err);
-			} else {
-				res.json(user);
-			}
-		});
+		res.json(user);
 	});
 };
 
--- public/modules/users/controllers/authentication.client.controller.js
+++ public/modules/users/controllers/authentication.client.controller.js
@@ -4,15 +4,14 @@ angular.module('users').controller('AuthenticationController', ['$scope', '$http
 	$scope.authentication = Authentication;
 
 	// If user is signed in then redirect back home
-	if ($scope.authentication.user) $location.path('/');
+	if ($scope.authentication.user) {
+		$location.path('/');
+	}
 
 	$scope.signup = function() {
 		$http.post('/auth/signup', $scope.credentials).success(function(response) {
-			// If successful we assign the response to the global user model
-			$scope.authentication.user = response;
-
-			// And redirect to the index page
-			$location.path('/');
+			$scope.error = undefined;
+			$scope.success = true;
 		}).error(function(response) {
 			$scope.error = response.message;
 		});
