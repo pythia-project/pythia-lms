1f3256c451394334f6a48a56ec4e536fa5b6f9b6
Sébastien Combéfis <seb478@gmail.com>
2015-11-19 18:38:11 +0100

    Change password reset URL with https

--- app/controllers/users/users.password.server.controller.js
+++ app/controllers/users/users.password.server.controller.js
@@ -58,7 +58,7 @@ exports.forgot = function(req, res, next) {
 			res.render('templates/reset-password-email', {
 				name: user.displayname,
 				appName: config.app.title,
-				url: 'http://' + req.headers.host + '/auth/reset/' + token
+				url: 'https://' + req.headers.host + '/auth/reset/' + token
 			}, function(err, emailHTML) {
 				done(err, emailHTML, user);
 			});
