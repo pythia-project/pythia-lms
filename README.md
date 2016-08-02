ff321aba955d46296d155c078195375e0ceb97bb
Sébastien Combéfis <seb478@gmail.com>
2016-08-02 16:09:48 +0200

    Add information about Pythia LMS

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Pythia-LMS
+
+Pythia LMS is an online platform hosting courses with Pythia problems.
+
+## Contributors
+
+- Sébastien Combéfis
--- config/env/default.js
+++ config/env/default.js
@@ -2,9 +2,9 @@
 
 module.exports = {
   app: {
-    title: 'MEAN.JS',
-    description: 'Full-Stack JavaScript with MongoDB, Express, AngularJS, and Node.js',
-    keywords: 'mongodb, express, angularjs, node.js, mongoose, passport',
+    title: 'Pythia LMS',
+    description: 'Pythia LMS is an online platform hosting courses with Pythia problems.',
+    keywords: 'Pythia, coding, grader',
     googleAnalyticsTrackingID: process.env.GOOGLE_ANALYTICS_TRACKING_ID || 'GOOGLE_ANALYTICS_TRACKING_ID'
   },
   port: process.env.PORT || 3000,
